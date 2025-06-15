0b5d3bbd578e97990c7414f6cdcfc68454c7eb4c
Denex A Poulose <denexdnx5@gmail.com>
2025-06-15 15:52:02 +0530

    Initial commit

---
+++ src/routes/transactionsRoute.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import {
+  createTransaction,
+  deleteTransaction,
+  getSummaryByUserId,
+  getTransactionsByUserId,
+} from "../controllers/transactionsController.js";
+
+const router = express.Router();
+router.get("/:userId", getTransactionsByUserId);
+
+router.delete("/:id", deleteTransaction);
+
+router.post("/", createTransaction);
+
+router.get("/summary/:userId", getSummaryByUserId);
+
+export default router;
